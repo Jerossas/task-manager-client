e888d7aff0fb25c468a7f3492a65255d8d81e98e
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-24 10:33:45 -0500

    refactor: reset app-root design

--- src/app/auth/sign-up/sign-up.ts
+++ src/app/auth/sign-up/sign-up.ts
@@ -1,9 +1,55 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { hasUpperCaseValidator, hasLowerCaseValidator, hasNumberValidator, hasSpecialCharacterValidator, passwordMatchValidator } from './validators/password.validators';
+import { AuthService } from '../auth-service';
+import { SignUpRequest } from '../models/sign-up-request.model';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
-export class SignUp {}
+export class SignUp {
+
+  protected signUpFormGroup: FormGroup;
+  protected hidePassword: boolean = true;
+  protected hideConfirmPassword: boolean = true;
+  protected genders: string[] = ['MALE', 'FEMALE'];
+
+  public constructor(private authService: AuthService) {
+    this.signUpFormGroup = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      middleName: new FormControl(''),
+      lastName: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
+        Validators.required, 
+        Validators.minLength(8),
+        hasUpperCaseValidator,
+        hasLowerCaseValidator,
+        hasNumberValidator,
+        hasSpecialCharacterValidator
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      gender: new FormControl('', [Validators.required]),
+    }, {
+      validators: passwordMatchValidator
+    });
+  }
+
+  protected onSubmit(): void {
+    if (this.signUpFormGroup.valid) {
+      const request: SignUpRequest = this.signUpFormGroup.value
+      this.authService.signUp(request).subscribe({
+        next: () => {
+          console.log('Sign-up successful');
+        },
+        error: (error) => {
+          console.error('Sign-up failed:', error);
+        }
+      });
+    }
+  }
+
+}
