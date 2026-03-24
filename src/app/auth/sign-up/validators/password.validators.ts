050ab1b04b347014e8a6c3a713cf09171afc135d
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-23 22:05:49 -0500

    feat: add password validators

---
+++ src/app/auth/sign-up/validators/password.validators.ts
@@ -0,0 +1,25 @@
+import { AbstractControl, ValidationErrors } from "@angular/forms";
+
+export function hasUpperCaseValidator(control: AbstractControl): ValidationErrors | null {
+  const value = control.value as string;
+  if (!value) return null;
+  return /[A-Z]/.test(value) ? null : { noUpperCase: true };
+}
+
+export function hasLowerCaseValidator(control: AbstractControl): ValidationErrors | null {
+  const value = control.value as string;
+  if (!value) return null;
+  return /[a-z]/.test(value) ? null : { noLowerCase: true };
+}
+
+export function hasNumberValidator(control: AbstractControl): ValidationErrors | null {
+  const value = control.value as string;
+  if (!value) return null;
+  return /\d/.test(value) ? null : { noNumber: true };
+}
+
+export function hasSpecialCharacterValidator(control: AbstractControl): ValidationErrors | null {
+  const value = control.value as string;
+  if (!value) return null;
+  return /[!@#$%^&*()-+]/.test(value) ? null : { noSpecialCharacter: true };
+}
