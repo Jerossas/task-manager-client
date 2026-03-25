3dc279984f1a25ffe2a099be0ac8beee12ce89f2
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-25 14:09:17 -0500

    feat: add routing for sign-up. Redirect to sign up in root for the moment.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SignUp } from './auth/sign-up/sign-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'sign-up',
+        component: SignUp
+    },
+    {
+        path: '',
+        redirectTo: 'sign-up',
+        pathMatch: 'full'
+    }
+];
